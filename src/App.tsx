5b0fce763b2e3b3509ddb0bdd060d042f82c1833
leozamboni <leonardonunes169@gmail.com>
2024-01-08 22:38:45 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -22,16 +22,16 @@ function App() {
         <Box textAlign='center' p='20px' w='100%' h='100%'>
           <Image src='./zamboni.png' h='60px' />
           <br />
-          <Text color='#006600' fontSize={{ lg: '6xl', base: '4xl' }} fontWeight='bold'>Você está <Text as="span" bg='#ff8080'>procurando</Text> por um fornecedor de <Text as="span" bg='#ff8080'>Morangos?</Text> <br /> Temos as <Text as="span" bg='#ff8080'>melhores ofertas</Text> para você!</Text>
-          <Text color='#333333' fontSize={{ lg: '2xl', base: 'xl' }}>Apoie a agricultura familiar com frutas selecionadas dos Campos de Cima da Serra no Rio Grande do Sul!</Text>
+          <Text color='#006600' fontSize={{ lg: '6xl', base: '4xl' }} fontWeight='bold'>Você está <Text as="span" bg='#ff8080'>procurando</Text> por um fornecedor de <Text as="span" bg='#ff8080'>morangos?</Text> <br /> Temos as <Text as="span" bg='#ff8080'>melhores ofertas</Text> para você!</Text>
+          <Text color='#333333' fontSize={{ lg: '2xl', base: 'xl' }}>Apoie a agricultura familiar, há mais de 14 anos trabalhando com frutas selecionadas nos Campos de Cima da Serra no Rio Grande do Sul!</Text>
           <br />
           <br />
           <Box w={{ lg: '50%', base: '80%' }} margin='auto' h={{ lg: '40%', base: '50%' }} display='block'>
             <SimpleGrid columns={{ lg: 2, base: 1 }} row={{ lg: 1, base: 2 }} spacing={5} w='100%' h='100%' >
-              <Box display='block' backgroundImage="url('/in-natura.jpg')" backgroundSize='cover' position='relative'>
+              <Box borderRadius='10px' display='block' backgroundImage="url('/in-natura.jpg')" backgroundSize='cover' position='relative'>
                 <Text textDecorationColor='#006600' position='relative' float='left' top='50%' left='50%' transform='translate(-50%,-50%)' color='white' fontSize='30pt'>In Natura</Text>
               </Box>
-              <Box display='block' backgroundImage="url('/congelado.jpg')" backgroundSize='cover' position='relative'>
+              <Box borderRadius='10px' display='block' backgroundImage="url('/congelado.jpg')" backgroundSize='cover' position='relative'>
                 <Text textDecorationColor='#006600' position='relative' float='left' top='50%' left='50%' transform='translate(-50%,-50%)' color='white' fontSize='30pt' >Congelado</Text>
               </Box>
             </SimpleGrid>
